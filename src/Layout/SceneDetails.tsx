555d6ab41d68fbc2a605fd419847c9266d3e4f8a
MattColu <mattcolu@gmail.com>
2024-11-14 19:55:53 +0100

    Vedi precedente
    Per qualche motivo mi ha separato i commit

--- src/Layout/SceneDetails.tsx
+++ src/Layout/SceneDetails.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Col, Form, InputGroup } from "react-bootstrap";
-import SuggestedTextArea from "./SuggestedTextArea";
+import SuggestedTextArea from "./SuggestedTextArea.tsx";
+import SceneDescription from "../StoryElements/SceneDescription.ts";
+import React from "react";
 
-function SceneDetails({sceneDescription}) {
+function SceneDetails(props: {sceneDescription: SceneDescription}) {
     const [weatherList, setWeatherList] = useState(["Soleggiato", "Velato", "Nuvoloso", "Pioggia", "Temporale"]);
     const [tonesList, setTonesList] = useState(["Felice", "Triste", "Arrabbiato", "Timoroso"]);
     const [valuesList, setValuesList] = useState(["Qualcosa", "Qualcos'altro"]);
 
-    const sd = sceneDescription;
     const textWidth = "25%";
 
     return(
@@ -21,40 +22,40 @@ function SceneDetails({sceneDescription}) {
                     <Form.Control
                         as="textarea"
                         style={{maxHeight:"10em"}}
-                        value={sd.summary}
-                        onChange={(e) => sd.setSummary(e.target.value)} />
+                        value={props.sceneDescription.summary}
+                        onChange={(e) => props.sceneDescription.setSummary(e.target.value)} />
                 </InputGroup>
                 <hr />
                 <InputGroup>
                     <InputGroup.Text style={{width: textWidth}}>Orario:</InputGroup.Text>
                     <Form.Control
-                        value={sd.time}
-                        onChange={(e) => sd.setTime(e.target.value)} />
+                        value={props.sceneDescription.time}
+                        onChange={(e) => props.sceneDescription.setTime(e.target.value)} />
                 </InputGroup>
                 <SuggestedTextArea
                     label="Tempo Atmosferico:"
-                    value={sd.weather}
-                    setValue={sd.setWeather}
+                    value={props.sceneDescription.weather}
+                    setValue={props.sceneDescription.setWeather}
                     choices={weatherList}
-                    onAdd={newWeather => setWeatherList([...weatherList, newWeather])}
+                    onAdd={(newWeather: string) => setWeatherList([...weatherList, newWeather])}
                     labelTextWidth={textWidth}>
                     +
                 </SuggestedTextArea>
                 <SuggestedTextArea
                     label="Tono:"
-                    value={sd.tone}
-                    setValue={sd.setTone}
+                    value={props.sceneDescription.tone}
+                    setValue={props.sceneDescription.setTone}
                     choices={tonesList}
-                    onAdd={newTone => setTonesList([...tonesList, newTone])}
+                    onAdd={(newTone: string) => setTonesList([...tonesList, newTone])}
                     labelTextWidth={textWidth}>
                     +
                 </SuggestedTextArea>
                 <SuggestedTextArea
                     label="Valore:"
-                    value={sd.value}
-                    setValue={sd.setValue}
+                    value={props.sceneDescription.value}
+                    setValue={props.sceneDescription.setValue}
                     choices={valuesList}
-                    onAdd={newValue => setValuesList([...valuesList, newValue])}
+                    onAdd={(newValue: string) => setValuesList([...valuesList, newValue])}
                     labelTextWidth={textWidth}>
                     +
                 </SuggestedTextArea>
